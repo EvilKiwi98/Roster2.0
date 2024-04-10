1aefd854d64df58b40ed6a8b8e587ab68ac59b1a
EvilKiwi98 <maxwell.shaun.0341@gmail.com>
2024-04-10 11:23:35 -0400

    Database Schema

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,11 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+--TEST DATA
+INSERT INTO crusade_cards (unit_name, unit_type, equipment, enhancements_upgrades, points_cost, crusade_points
+experience_points, model_count, battles_played, battles_survived, units_destroyed, battle_honors, battle_scars,
+rank)
+VALUES ('Grand Vizier Mahkret', 'Plasmancer', 'Plasmic Lance', 'Dimensional Sanctum', 75, 2, 8, 1, 2, 1, 2,
+'Trait - Endless Legions. Relic - Godpyre Mantle', 'Blooded');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, crusade_cards CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,26 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
+	);
+
+CREATE TABLE crusade_cards (
+	unit_id SERIAL,
+	unit_name varchar(50) NOT NULL UNIQUE,
+	unit_type varchar(50) NOT NULL,
+	equipment varchar (150),
+	enhancements_upgrades varchar(150),
+	points_cost INT NOT NULL,
+	crusade_points INT NOT NULL,
+	experience_points INT,
+	model_count INT,
+	battles_played INT,
+	battles_survived INT,
+	units_destroyed INT,
+	battle_honors varchar(1000),
+	battle_scars varchar(500),
+	rank varchar(50)
+	);
+
+
 
 COMMIT TRANSACTION;
